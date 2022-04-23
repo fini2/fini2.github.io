90661f3fddab36b84ac71629113fbcb752078e83
Hasnain Saeed <69143815+hasnain-saeed@users.noreply.github.com>
2022-04-24 02:09:10 +0500

    Fix title

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <h1>
           fini2
-        </p>
+        </h1>
         <p>
           COMING SOON
         </p>
